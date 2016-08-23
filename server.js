f6be04115f417044c3136a5a434d8af9300f0e9c
jerome amos <mail@amosdesigns.net>
2016-08-23 15:33:44 -0400

    added get request for all and id

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ var express = require('express'),
         id: 2,
         description: 'do node course for the next 2 weeks',
         completed: false
+    }, {
+        id: 3,
+        description: 'go to the apple store for a new computer',
+        completed: true
     }],
     app = express();
 
@@ -26,6 +30,28 @@ app.get('/',  function (req, res) { //middleware.requireAuthentication
 });
 
 // app.use(express.static(__dirname +'/public'));
+// GET /todos
+
+app.get('/todos',  function (req, res) { //middleware.requireAuthentication
+    res.json(todos);
+});
+
+//GET /todos:id
+app.get('/todos/:id',  function (req, res) { //middleware.requireAuthentication
+    var todoId = parseInt(req.params.id, 10),
+        matchedTodo;
+    todos.forEach(function (todo) {
+        if(todoId === todo.id){
+            matchedTodo = todo;
+        }
+    });
+    if(matchedTodo){
+        res.send('asking for todo with ID of: ' + todoId);
+        res.json(todos);
+    } else {
+        res.status(404).send();
+    }
+});
 
 app.listen(PORT, function () {
     console.log('Express Server started on PORT: '+PORT+"!");
