f1855bd0350cde1fdc933d51dadcb3d4894deefa
jerome amos <mail@amosdesigns.net>
2016-08-24 09:57:50 -0400

    refactor post

--- server.js
+++ server.js
@@ -42,11 +42,12 @@ app.get('/todos/:id', function (req, res) { //middleware.requireAuthentication
 
 //POST /todos
 app.post('/todos', function (req, res) {
-        var body = req.body;
+        var body = _.pick(req.body, 'description', 'completed');
         if (!_.isBoolean(body.completed) || !_.isString(body.description || body.description.trim().length === 0)) {
             res.status(400)
                .send();
         }
+        body.description = body.description.trim();
         body.id = todoNextId++;
         todos.push(body);
         res.json(body);
