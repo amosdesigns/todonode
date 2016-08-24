bfb4ace0e002fb27f7574bbe5c5d87b8d6f1ca3c
jerome amos <mail@amosdesigns.net>
2016-08-24 12:33:03 -0400

    added playground

---
+++ variable-playground.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Jerome on 8/24/16.
+ */
+// var person = {
+//     name:'jerome',
+//     age: 48
+// };
+//
+//
+// function updatePerson(obj) {
+//     obj = {
+//         name:'jerome',
+//         age: 84
+//     };
+//     obj.age = 24;
+// }
+// updatePerson(person);
+// console.log(person);
+
+
+//array Example
+var grades = [15,48];
+function addGrade(gradesArr) {
+  //gradesArr.push(93);
+    gradesArr = [12,34,56];
+debugger;
+}
+addGrade(grades);
+console.log(grades);
