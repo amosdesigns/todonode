a6e0aa85064bc1017394db9ad1400128a53a8174
jerome amos <mail@amosdesigns.net>
2016-08-25 11:15:14 -0400

    updated search opt

--- server.js
+++ server.js
@@ -28,23 +28,29 @@ app.get('/', function (req, res) { //middleware.requireAuthentication
 // GET /todos?completed=true&q=work
 app.get('/todos', function (req, res) { //middleware.requireAuthentication
     "use strict";
-    var queryParams = req.query,
-        filteredTodos = todos;
+    var query = req.query,
+        where = {};
 
-    if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-        filteredTodos = _.where(filteredTodos, {completed: true});
-    } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-        filteredTodos = _.where(filteredTodos, {completed: false});
+    if (query.hasOwnProperty('completed') && query.completed === 'true') {
+        where.completed = true;
+    } else if (query.hasOwnProperty('completed') && query.completed === 'false') {
+        where.completed = false;
     }
+
     // search filter - "go to work on saturday"
-    if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-        filteredTodos = _.filter(filteredTodos, function (todo) {
-            "use strict";
-            return todo.description.toLowerCase()
-                       .indexOf(queryParams.q.toLowerCase()) > -1;
-        });
+    if (query.hasOwnProperty('q') && query.q.length > 0) {
+        where.description = {
+            $like: '%' + query.q + '%'
+        };
     }
-    res.json(filteredTodos);
+
+    db.todo.findAll({where: where})
+      .then(function (todos) {
+          res.json(todos);
+      }, function (e) {
+          res.status(500)
+             .send();
+      });
 });
 
 //GET /todos:id
